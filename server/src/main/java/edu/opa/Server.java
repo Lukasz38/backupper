99ba0eca19efb99733722d28d03fe93ed4b20c58
Łukasz Świątkowski <luk.swiatkowski@gmail.com>
2017-11-26 15:14:32 +0100

    Load server config from file

--- server/src/main/java/edu/opa/Server.java
+++ server/src/main/java/edu/opa/Server.java
@@ -1,7 +1,14 @@
 package edu.opa;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URISyntaxException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
 
 import org.apache.ftpserver.FtpServer;
 import org.apache.ftpserver.FtpServerFactory;
@@ -12,51 +19,97 @@ import org.apache.ftpserver.listener.ListenerFactory;
 import org.apache.ftpserver.usermanager.PropertiesUserManagerFactory;
 import org.apache.ftpserver.usermanager.impl.BaseUser;
 import org.apache.ftpserver.usermanager.impl.WritePermission;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Server {
 
+	protected static final Logger log = LoggerFactory.getLogger(Server.class);
+	
+	private static final String CONFIG_FILE_NAME = "config.properties";
+	
+	private FtpServer server;
+	private int port;
+	private String username;
+	private String password;
+	private String homeDir;
+	
 	public static void main(String[] args) {
+		Server server = new Server();
+		server.start();
+	}
+	
+	public void start()
+	{
+		loadConfig();
+		init();
+		try {
+			server.start();
+			log.info("Server is up.");
+		} catch (FtpException e) {
+			log.error("FTP server exception.\n"
+					+ "Exception message: {}", e);
+		}	
+	}
+	
+	private void init()
+	{
 		FtpServerFactory serverFactory = new FtpServerFactory();
 		ListenerFactory factory = new ListenerFactory();
-		// set the port of the listener
-		factory.setPort(2223);
-		// replace the default listener
+		factory.setPort(port);
 		serverFactory.addListener("default", factory.createListener());
-		
-		
-		
+
 		PropertiesUserManagerFactory userManagerFactory = new PropertiesUserManagerFactory();
-	
-		UserManager um = userManagerFactory.createUserManager();
+		UserManager userManager = userManagerFactory.createUserManager();
 		BaseUser user = new BaseUser();
-		user.setName("luk");
-		user.setPassword("luk");
-		user.setHomeDirectory("D:/FTP");
+		user.setName(username);
+		user.setPassword(password);
+		user.setHomeDirectory(homeDir);
 		
 		List<Authority> authorities = new ArrayList<Authority>();
 		authorities.add(new WritePermission());
 		user.setAuthorities(authorities);
-		try
-		{
-			um.save(user);
-		} catch (FtpException e1)
-		{
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
+		try	{
+			userManager.save(user);
+		} catch (FtpException e) {
+			log.error("Exception while adding user.\n"
+					+ "Exception message: {}", e);
 		}
+		serverFactory.setUserManager(userManager);
+		server = serverFactory.createServer();   
 		
-		// start the server
-		serverFactory.setUserManager(um);
-		FtpServer server = serverFactory.createServer();   
-		
-		try
-		{
-			server.start();
-		} catch (FtpException e)
-		{
-			// TODO Auto-generated catch block
-			server.stop();
-			e.printStackTrace();
+		log.info("Server initialized.");
+	}
+	
+	private void loadConfig()
+	{
+		Properties prop = new Properties();
+
+		URL url = this.getClass().getResource("/" + CONFIG_FILE_NAME);
+		if(url == null) {
+			log.error("Configuration file not found: " + CONFIG_FILE_NAME);
+			throw new RuntimeException();
 		}
+		try(InputStream input = new FileInputStream(
+				new File(url.toURI()))) {
+
+			prop.load(input);
+
+			port = Integer.parseInt(prop.getProperty("port"));
+			username = prop.getProperty("user");
+			password = prop.getProperty("password");
+			homeDir = prop.getProperty("home.dir");
+			
+		} catch (IOException | URISyntaxException e) {
+			log.error("Exception while loading configuration from config.properties.\n"
+					+ "Exception message: {}", e);
+			throw new RuntimeException();
+		} 
+		log.info("Configuration loaded:\n"
+				+ "port: {}\n"
+				+ "user: {}\n"
+				+ "password: {}\n"
+				+ "homeDir: {}",
+				port, username, password, homeDir);
 	}
 }
